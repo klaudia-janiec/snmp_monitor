43aa40a1d4a2c0d0dd8e26ebd737b5a3738ef89d
klaudia <klaudia.janiec@gmail.com>
2018-04-20 10:26:05 +0200

    Improve displaying data

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -4,6 +4,12 @@ require_relative "base_controller"
 require_relative "../services/system_overview"
 
 class AgentsController < BaseController
+  helpers do
+    def to_gb(size)
+      (size / 1_000_000.0).round(2)
+    end
+  end
+
   namespace "/agents" do
     get "/" do
       locals = SystemOverview.new.call
